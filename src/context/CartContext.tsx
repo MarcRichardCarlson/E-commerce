c4b8eac63ad533c5f5380880a9e016a54f517729
Marc Carlson <MarcRichardCarlson@users.noreply.github.com>
2023-09-15 08:39:11 +0200

    comments

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -26,43 +26,52 @@ export const CartProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }
   // State for cart items
   const [cartItems, setCartItems] = useState<any[]>([]);
 
-  // Add an item to the cart
-  // Add an item to the cart
+ // Add an item to the cart or increment its quantity if it already exists
 const addToCart = (item: ProductDetailsProps) => {
+    // Check if the item already exists in the cart by searching for its 'id'.
   const existingItemIndex = cartItems.findIndex((cartItem) => cartItem.id === item.id);
 
-    if (existingItemIndex !== -1) {
-      // If the item already exists in the cart, increment its quantity
-      const updatedCartItems = [...cartItems];
-      updatedCartItems[existingItemIndex] = {
-        ...updatedCartItems[existingItemIndex],
-        quantity: updatedCartItems[existingItemIndex].quantity + 1,
-      };
-      setCartItems(updatedCartItems);
-    } else {
-      // If the item doesn't exist in the cart, add it with quantity 1
-      setCartItems([...cartItems, { ...item, quantity: 1 }]);
-    }
-  };
+  if (existingItemIndex !== -1) {
+    // If the item already exists in the cart, increment its quantity
+    const updatedCartItems = [...cartItems];
+    updatedCartItems[existingItemIndex] = {
+      ...updatedCartItems[existingItemIndex],
+      quantity: updatedCartItems[existingItemIndex].quantity + 1,
+    };
+    // Update the cart with the modified cartItems.
+    setCartItems(updatedCartItems);
+  } else {
+    // If the item doesn't exist in the cart, add it with quantity 1
+    setCartItems([...cartItems, { ...item, quantity: 1 }]);
+  }
+};
 
 
   // Remove an item from the cart
   const removeFromCart = (itemId: string) => {
+    // Map through cartItems, increment 'quantity' by 1 for the item with a matching id, or keep it unchanged
     const updatedCart = cartItems.filter((item) => item.id !== itemId);
+    // Update the cart with the modified cartItems.
     setCartItems(updatedCart);
   };
 
   // Get the quantity of a specific item in the cart
   const getItemQuantity = (itemId: string) => {
+    //Uses find method on array cartItems, searching for item with id property matching provided itemId
     const item = cartItems.find((item) => item.id === itemId);
+    //if item found, return item.quantity else 0
     return item ? item.quantity : 0;
   };
 
   // Increase the quantity of a specific item in the cart
   const increaseCartQuantity = (itemId: string) => {
+    /* Uses map method on cartItems, checks if id matches provided itemId. If matching, 
+    then create new object with same properties as the item but increment 'quantity' by 1.
+    Else keep unchanged */
     const updatedCart = cartItems.map((item) =>
       item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
     );
+    // Update the cart with the modified cartItems.
     setCartItems(updatedCart);
   };
 
@@ -71,6 +80,7 @@ const addToCart = (item: ProductDetailsProps) => {
     const updatedCart = cartItems.map((item) =>
       item.id === itemId && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
     );
+    // Update the cart with the modified cartItems.
     setCartItems(updatedCart);
   };
 
